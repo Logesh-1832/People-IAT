3fadd66e622bd79d82cad9eca79701ff1f514f0b
Logesh <logeshkumar18320@gmail.com>
2024-01-22 11:53:55 +0530

    Added First Trial

---
+++ my-app/src/app/First-Trial/first-trial/first-trial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-trial',
+  standalone: true,
+  imports: [],
+  templateUrl: './first-trial.component.html',
+  styleUrl: './first-trial.component.css'
+})
+export class FirstTrialComponent {
+
+}
