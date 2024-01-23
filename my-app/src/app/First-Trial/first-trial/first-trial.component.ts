a4dcf7ab92a23b231b997050b97db78eddece879
Logesh <logeshkumar18320@gmail.com>
2024-01-23 22:46:34 +0530

    Added Event Key listener

--- my-app/src/app/First-Trial/first-trial/first-trial.component.ts
+++ my-app/src/app/First-Trial/first-trial/first-trial.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-first-trial',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './first-trial.component.html',
   styleUrl: './first-trial.component.css'
 })
 export class FirstTrialComponent {
-text:string = 'Part 1 of 4';
+text:string = '';
+isEnabled: boolean = true;
+isEnableWords: boolean = false;
 
+constructor() {
+   this.text
+}
+
+@HostListener('window:keyup', ['$event'])
+keyEvent(event: KeyboardEvent) {
+  if(event.key == " "){
+    this.isEnabled = false;
+    this.isEnableWords = true;
+    this.getDummy();
+  }
+}
+
+  getDummy(){
+    this.text = "BAD";
+  }
 }
