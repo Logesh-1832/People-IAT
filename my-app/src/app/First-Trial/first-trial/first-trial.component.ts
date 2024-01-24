49f5d58c4514d7c3768577565d563fb4217b1f93
Logesh <logeshkumar18320@gmail.com>
2024-01-24 22:20:54 +0530

    Added functionalitiy for first trial

--- my-app/src/app/First-Trial/first-trial/first-trial.component.ts
+++ my-app/src/app/First-Trial/first-trial/first-trial.component.ts
@@ -10,9 +10,33 @@ import { Component, HostListener } from '@angular/core';
 })
 export class FirstTrialComponent {
 text:string = '';
+positveArray:any = [
+  {name:"Enjoy",value:"e"},
+  {name:"Pleasing",value:"e"},
+  {name:"Excellent",value:"e"}, 
+  {name:"Laughing",value:"e"}, 
+  {name:"Celebrate",value:"e"}, 
+  {name:"Love",value:"e"},
+  {name:"Fantastic",value:"e"},
+  {name:"Lovely",value:"e"},
+];
+
+negativeArray:any = [
+  {name:"Hate",value:"i"},
+  {name:"Poison",value:"i"},
+  {name:"Nasty",value:"i"}, 
+  {name:"Disgust",value:"i"}, 
+  {name:"Horrific",value:"i"}, 
+  {name:"Abuse",value:"i"},
+  {name:"Pain",value:"i"},
+  {name:"Detest",value:"i"},
+];
+bothArray:any = [ "Hate", "Poison", "Nasty", "Disgust", "Horrific", "Abuse", "Pain", "Detest" , "Enjoy", "Pleasing", "Excellent", "Laughing", "Celebrate", "Love", "Fantastic", "Lovely" ]
+
 isEnabled: boolean = true;
 isEnableWords: boolean = false;
-
+date:any | undefined;
+istrue: boolean = false;
 constructor() {
    this.text
 }
@@ -22,11 +46,63 @@ keyEvent(event: KeyboardEvent) {
   if(event.key == " "){
     this.isEnabled = false;
     this.isEnableWords = true;
-    this.getDummy();
+    this.text = this.positveArray[0].name;
+  }
+
+  if(event.key.toLowerCase() == "e"){
+    this.isEnabled = false;
+    this.isEnableWords = true;
+    this.checkPositive();
+  }
+
+  if(event.key.toLowerCase() == "i"){
+    this.isEnabled = false;
+    this.isEnableWords = true;
+    this.checkNegative();
+  }
+
+ }
+
+  checkPositive(){
+    this.istrue = false;
+    let a = this.positveArray.filter( (x: { name: string; }) => x.name == this.text);
+    if(a.length != 0){
+      if(this.text == a[0].name){
+        this.getRandomWords();
+      }
+    }
+    else{
+      this.istrue = true;
+    }
+    // this.date = Date.now()
+    // console.log(this.date);
+    // for(let i = 0; i<this.arr.length; i++){
+    //   this.text = this.arr[i];
+    // }
+  }
+
+  checkNegative(){
+    this.istrue = false;
+    let a = this.negativeArray.filter( (x: { name: string; }) => x.name == this.text);
+    if(a.length != 0){
+      if(this.text == a[0].name){
+        this.getRandomWords();
+      }
+    }
+    else{
+      this.istrue = true;
+    }
+  }
+
+  getRandomWords(){
+    var ri = Math.floor(Math.random() * this.bothArray.length);
+    var rs = this.bothArray.splice(ri, 1);
+    if( rs.length != 0){
+      this.text = rs[0];
+    }
+    else{
+      this.text = " ";
+    }
   }
 }
 
-  getDummy(){
-    this.text = "BAD";
-  }
-}
